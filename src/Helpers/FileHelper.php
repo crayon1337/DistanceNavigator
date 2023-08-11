fe2075c19c672cba515adc141bc38948f7c54f68
Mohamed Mahmoud <mohammedarey2@gmail.com>
2023-08-11 20:56:27 +0300

    Refactoring Geolocation service

--- src/Helpers/FileHelper.php
+++ src/Helpers/FileHelper.php
@@ -4,14 +4,22 @@ namespace App\Helpers;
 
 class FileHelper
 {
-    public static function export(string $fileName, array $data): void
+    public static function export(string $fileName, array $data, array $headers): void
     {
         $fp = fopen($fileName, 'w');
+        $data = self::prependHeaders($data, $headers);
 
         foreach ($data as $row) {
             fputcsv($fp, $row, ',');
         }
 
         fclose($fp);
+    }
+
+    private static function prependHeaders(array $data, array $headers)
+    {
+        array_unshift($data, $headers);
+
+        return $data;
     }
 }
